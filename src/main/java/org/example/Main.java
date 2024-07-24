30559031a5182cb58a32a22fbd080f5bc62e4b3b
kislian <kisli_an@mail.ru>
2024-07-24 13:22:45 +0800

    edit HW_algorithmi_3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Домашнее задание 2.15. Основы Алгоритмов часть 3");
+        int[] array = SortIntegerMassiv.getMassivRandomValues(100_000);
+
+        long start = System.currentTimeMillis();
+        SortIntegerMassiv.sortBubble(Arrays.copyOf(array, array.length));
+        System.out.println(System.currentTimeMillis() - start);
+
+        long start2 = System.currentTimeMillis();
+        SortIntegerMassiv.sortSelection(Arrays.copyOf(array, array.length));
+        System.out.println(System.currentTimeMillis() - start2);
+
+        long start3 = System.currentTimeMillis();
+        SortIntegerMassiv.sortInsertion(Arrays.copyOf(array, array.length));
+        System.out.println(System.currentTimeMillis() - start3);
     }
 }
---
+++ src/main/java/org/example/SortIntegerMassiv.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.Random;
+
+public class SortIntegerMassiv {
+    private static void swapElements(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    public static void sortBubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapElements(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void sortSelection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    minElementIndex = j;
+                }
+            }
+            swapElements(arr, i, minElementIndex);
+        }
+    }
+
+    public static void sortInsertion(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    public static int[] getMassivRandomValues(int count) {
+        int[] arr = new int[count];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt();
+        }
+        return arr;
+    }
+}
